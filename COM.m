069453551e77d69f39a5803d68f204213572d0ca
stieber <45106461+stieber@users.noreply.github.com>
2018-11-16 13:48:46 -0500

    Update COM.m

--- COM.m
+++ COM.m
@@ -3,6 +3,7 @@ function [x,y,z] = COM(R,M)
 %   R is N-by-3 matrix of positions corresponding to N bodies
 %   M is N-by-1 column vector of masses corresponding to N bodies (constant
 %   mass assumed)
+% test change
 X = 0; Y = 0; Z = 0;
 for n = 1:length(M)
     X = X + R(n,1)*M(n);
